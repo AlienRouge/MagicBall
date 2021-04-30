1e368f4260e01bf072d19051bbb1cc25e33b19ba
AlienRouge <rudov.vlad2013@gmail.com>
2021-04-30 17:18:54 +0700

    Add project files.

---
+++ MagicBall/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace MagicBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.questionBox = new System.Windows.Forms.TextBox();
+            this.yesLimit = new System.Windows.Forms.NumericUpDown();
+            this.AnsBtn = new System.Windows.Forms.Button();
+            this.SwitchBtn = new System.Windows.Forms.Button();
+            this.answerBox = new System.Windows.Forms.Label();
+            this.YesLimitLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.yesLimit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // questionBox
+            // 
+            this.questionBox.Location = new System.Drawing.Point(43, 82);
+            this.questionBox.Name = "questionBox";
+            this.questionBox.Size = new System.Drawing.Size(251, 20);
+            this.questionBox.TabIndex = 0;
+            // 
+            // yesLimit
+            // 
+            this.yesLimit.DecimalPlaces = 1;
+            this.yesLimit.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.yesLimit.Location = new System.Drawing.Point(94, 265);
+            this.yesLimit.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.yesLimit.Name = "yesLimit";
+            this.yesLimit.Size = new System.Drawing.Size(52, 20);
+            this.yesLimit.TabIndex = 1;
+            this.yesLimit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.yesLimit.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // AnsBtn
+            // 
+            this.AnsBtn.Font = new System.Drawing.Font("Perpetua Titling MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AnsBtn.Location = new System.Drawing.Point(110, 120);
+            this.AnsBtn.Name = "AnsBtn";
+            this.AnsBtn.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.AnsBtn.Size = new System.Drawing.Size(111, 59);
+            this.AnsBtn.TabIndex = 2;
+            this.AnsBtn.Text = "I want to know";
+            this.AnsBtn.UseVisualStyleBackColor = true;
+            this.AnsBtn.Click += new System.EventHandler(this.AnsBtn_Click);
+            // 
+            // SwitchBtn
+            // 
+            this.SwitchBtn.Location = new System.Drawing.Point(261, 262);
+            this.SwitchBtn.Name = "SwitchBtn";
+            this.SwitchBtn.Size = new System.Drawing.Size(60, 23);
+            this.SwitchBtn.TabIndex = 3;
+            this.SwitchBtn.Text = "Yes/No";
+            this.SwitchBtn.UseVisualStyleBackColor = true;
+            this.SwitchBtn.Click += new System.EventHandler(this.SwitchBtn_Click);
+            // 
+            // answerBox
+            // 
+            this.answerBox.AutoSize = true;
+            this.answerBox.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerBox.Location = new System.Drawing.Point(14, 211);
+            this.answerBox.MaximumSize = new System.Drawing.Size(500, 500);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.answerBox.Size = new System.Drawing.Size(69, 20);
+            this.answerBox.TabIndex = 4;
+            this.answerBox.Text = "label1";
+            this.answerBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // YesLimitLabel
+            // 
+            this.YesLimitLabel.AutoSize = true;
+            this.YesLimitLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.YesLimitLabel.Location = new System.Drawing.Point(6, 267);
+            this.YesLimitLabel.Name = "YesLimitLabel";
+            this.YesLimitLabel.Size = new System.Drawing.Size(88, 15);
+            this.YesLimitLabel.TabIndex = 5;
+            this.YesLimitLabel.Text = "Yes/No limit:";
+            this.YesLimitLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Magic Ball";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Answer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 295);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.YesLimitLabel);
+            this.Controls.Add(this.answerBox);
+            this.Controls.Add(this.SwitchBtn);
+            this.Controls.Add(this.AnsBtn);
+            this.Controls.Add(this.yesLimit);
+            this.Controls.Add(this.questionBox);
+            this.Name = "Form1";
+            this.Text = "MagicBall";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.yesLimit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox questionBox;
+        private System.Windows.Forms.NumericUpDown yesLimit;
+        private System.Windows.Forms.Button AnsBtn;
+        private System.Windows.Forms.Button SwitchBtn;
+        private System.Windows.Forms.Label answerBox;
+        private System.Windows.Forms.Label YesLimitLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MagicBall/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MagicBall
+{
+    public partial class Form1 : Form
+    {
+        private bool _isYesMode;
+        private MagicBall _magicBall = new MagicBall();
+
+        public Form1()
+        {
+            InitializeComponent();
+            yesLimit.Visible = YesLimitLabel.Visible = false;
+            answerBox.Text = "";
+        }
+
+        public class MagicBall
+        {
+          
+            public double yesLimit = 0.5;
+
+            private readonly Dictionary<int, string> _answers = new Dictionary<int, string>()
+            {
+                {0, "Don’t count on it."}, 
+                {1, "My reply is no."}, 
+                {2, "My sources say no."}, 
+                {3, "Outlook not so good."}, 
+                {4, "Very doubtful."},
+
+                {5, "Reply hazy, try again."},
+                {6, "Ask again later."},
+                {7, "Better not tell you now."},
+                {8, "Cannot predict now."},
+                {9, "Concentrate and ask again."},
+
+                {10, "It is certain."},
+                {11, "It is decidedly so."},
+                {12, "Without a doubt."},
+                {13, "Yes – definitely."},
+                {14, "You may rely on it."},
+                {15, "As I see it, yes."},
+                {16, "Most likely."},
+                {17, "Outlook good."},
+                {18, "Yes."},
+                {19, "Signs point to yes."}
+            };
+
+            private readonly Dictionary<int, double> _probability = new Dictionary<int, double>()
+            {
+                {0, 0.1},
+                {1, 0.01},
+                {2, 0.05},
+                {3, 0.045},
+                {4, 0.045},
+
+                {5, 0.045},
+                {6, 0.05},
+                {7, 0.1},
+                {8, 0.045},
+                {9, 0.01},
+
+                {10, 0.045},
+                {11, 0.045},
+                {12, 0.01},
+                {13, 0.05},
+                {14, 0.1},
+                {15, 0.05},
+                {16, 0.045},
+                {17, 0.01},
+                {18, 0.1},
+                {19, 0.045}
+            };
+
+            public string RollBall()
+            {
+                Random random =  new Random();
+                double Prob = random.NextDouble();
+                for (var i = 0; i <= _probability.Count; i++)
+                {
+                    Prob -= _probability[i];
+                    if (Prob <= 0) 
+                        return _answers[i];
+                };
+                return null;
+            }
+        }
+
+        private void SwitchBtn_Click(object sender, EventArgs e)
+        {
+            if (!_isYesMode)
+            {
+                _isYesMode = yesLimit.Visible = YesLimitLabel.Visible = true;
+            }
+            else
+            {
+                _isYesMode = yesLimit.Visible = YesLimitLabel.Visible = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AnsBtn_Click(object sender, EventArgs e)
+        {
+            if (questionBox.Text != "")
+            {
+                if (_isYesMode)
+                {
+                    Random random = new Random();
+                    answerBox.Text = random.NextDouble() > (double) yesLimit.Value ? "Yes!" : "No.";
+                }
+                else
+                {
+                    answerBox.Text = _magicBall.RollBall();
+                }
+            }
+            else
+            {
+                answerBox.Text = "Where is your question?";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
